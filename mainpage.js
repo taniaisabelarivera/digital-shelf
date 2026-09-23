92e6506125dbe79aa3be535cdea800d2436d4096
taniaisabelarivera <rivera.tania275@csu.fullerton.edu>
2026-09-23 00:04:10 -0700

    More minor tweaks to index.html and mainpage.js, added <script> to index and fixed variable name in mainpage

--- mainpage.js
+++ mainpage.js
@@ -1,4 +1,4 @@
-const KEY = import.meta.env.VITE_API_KEY;
+const KEY = import.meta.env.VITE_TMDB_API_KEY;
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 const BASE = "https://api.themoviedb.org/3";
 
